3c6215ef8b09b6a832ac23a29b8977111ecb6880
Gld_Mx24483 <israeltoki7@gmail.com>
2024-11-28 03:19:05 +0100

    scrapays mobile app

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
+// module.exports = {
+//   presets: ['module:@react-native/babel-preset'],
+//   plugins: ['react-native-reanimated/plugin'],
+// };
+
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    // Add this if you're using TypeScript
+    '@babel/plugin-transform-typescript'
+  ],
 };
